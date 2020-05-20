8040ae635fadf72918edade08f5e894d51a71cce
Prashant <prashantgaikwad1997@gmail.com>
2020-05-20 19:09:07 +0530

    Made get_data more variable

--- Tech_analysis/get_data.py
+++ Tech_analysis/get_data.py
@@ -2,8 +2,8 @@ import pandas_datareader.data as web
 import pandas as pd
 
 
-def data(ticker, start, end, name=None):
+def data(ticker, start, end, source='yahoo', loc='stonk_dfs', name=None):
     if name is None:
         name = ticker
-    df = web.DataReader(ticker, 'yahoo', start, end)
-    df.to_csv('stonk_dfs/{}.csv'.format(name))
+    df = web.DataReader(ticker, source, start, end)
+    df.to_csv(loc + '/{}.csv'.format(name))
